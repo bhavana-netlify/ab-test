6f89c343174bea0dc5a9306c26e91c4a1e19c449
Henry Smith <henry.smith@netlify.com>
2022-12-14 13:44:37 +0000

    Use Next rewrite and check status

--- middleware.js
+++ middleware.js
@@ -54,12 +54,13 @@ export const middleware = async (nextRequest) => {
       if (pathname.startsWith("/_next")) return response;
 
       let path = `${origin}/${bucket}${pathname}`;
+
       console.log("Checking Path:", path);
-      const res = await fetch(`${path}?check=true`, { method: "HEAD" });
+      let res = await NextResponse.rewrite(`${path}?check=true`);
+
       console.log("status", res.status);
       if (res.status < 400) {
-        console.log("Path Found");
-        return NextResponse.rewrite(path);
+        return res;
       } else {
         console.log("Path not found");
       }
