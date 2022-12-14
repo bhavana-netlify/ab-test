f011735ae1ca0210ce8aada06bd919f6f36bf544
Henry Smith <henry.smith@netlify.com>
2022-12-14 13:26:23 +0000

    Try get

--- middleware.js
+++ middleware.js
@@ -56,7 +56,7 @@ export const middleware = async (nextRequest) => {
       let path = `${origin}/${bucket}${pathname}`;
       console.log("Checking Path:", path);
 
-      const res = await fetch(`${path}?check=true`, { method: "HEAD" });
+      const res = await fetch(`${path}?check=true`);
 
       console.log("status", res.status);
       if (res.status < 400) {
